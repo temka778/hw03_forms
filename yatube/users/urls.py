48bbcb002d930aefa479ad4d7a4adc9506bb4e0e
Артём <av1965k@yandex.ru>
2022-10-30 19:46:25 +0400

    устранение достатков 11

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -16,47 +16,33 @@ app_name = 'users'
 
 
 urlpatterns = [
-    path(
-      'logout/',
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
-    ),
+    path('logout/',
+         LogoutView.as_view(template_name='users/logged_out.html'),
+         name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path(
-      'login/',
-      LoginView.as_view(template_name='users/login.html'),
-      name='login'
-    ),
-    path(
-      'password_reset/done/',
-      PasswordResetDoneView.as_view(
-        template_name='users/password_reset_done.html'),
-      name='password_reset_done'
-    ),
-    path(
-      'password_reset/',
-      PasswordResetView.as_view(
-        template_name='users/password_reset_form.html'),
-      name='password_reset'
-    ),
-    path(
-      'password_change/done/',
-      PasswordChangeDoneView.as_view(
-        template_name='users/password_change_done.html'),
-      name='password_change_done'
-    ),
-    path(
-      'password_change/',
-      PasswordChangeView.as_view(
-        template_name='users/password_change_form.html'),
-      name='password_change'
-    ),
-    path(
-      'reset/done/',
-      PasswordResetCompleteView.as_view(
-        template_name='users/password_reset_complete.html'),
-      name='password_reset_complete'
-    ),
+    path('login/',
+         LoginView.as_view(template_name='users/login.html'),
+         name='login'),
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(
+             template_name='users/password_reset_done.html'),
+         name='password_reset_done'),
+    path('password_reset/',
+         PasswordResetView.as_view(
+             template_name='users/password_reset_form.html'),
+         name='password_reset'),
+    path('password_change/done/',
+         PasswordChangeDoneView.as_view(
+             template_name='users/password_change_done.html'),
+         name='password_change_done'),
+    path('password_change/',
+         PasswordChangeView.as_view(
+             template_name='users/password_change_form.html'),
+         name='password_change'),
+    path('reset/done/',
+         PasswordResetCompleteView.as_view(
+             template_name='users/password_reset_complete.html'),
+         name='password_reset_complete'),
     path('reset/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(
              template_name='users/password_reset_confirm.html'),
