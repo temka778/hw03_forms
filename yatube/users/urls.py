6a1237064a1668cb7a1f9e3a5a5ac9c5b8da8c5b
Артём <av1965k@yandex.ru>
2022-10-30 19:24:14 +0400

    устранение достатков 8

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -60,6 +60,5 @@ urlpatterns = [
     path('reset/<uidb64>/<token>/',
          PasswordResetConfirmView.as_view(
           template_name='users/password_reset_confirm.html'),
-        name='password_reset_confirm'
-    )
+         name='password_reset_confirm')
 ]
